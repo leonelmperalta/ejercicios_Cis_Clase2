d401b6c4f47d7d7beb25e9779a4b482130178f17
leonelmperalta <54342655+leonelmperalta@users.noreply.github.com>
2020-08-27 16:33:23 -0300

    Add files via upload

---
+++ elije_tu_propia_aventura/historia/funcion.js
@@ -0,0 +1,8 @@
+function evaluarCodigo(){
+        if(document.getElementById("codigo").value == "237"){
+            window.location.href = "final_feliz.html";
+        }
+        else{
+            window.location.href = "final_malo.html";
+        }
+}
